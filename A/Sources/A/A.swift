659abbb1e42efdc92f469274ba6a2a368560e9be
Michael Schwarz <michael_schwarz@me.com>
2020-06-05 12:36:21 +0200

    Extend samples to fail with generated header module error

--- A/Sources/A/A.swift
+++ A/Sources/A/A.swift
@@ -8,3 +8,15 @@ public struct A {
 public class AObjC: NSObject {
     @objc public static var text = "Hello, World!"
 }
+
+@objc
+public protocol AProtObjc: AnyObject {
+    var text: String { get }
+}
+
+@objc
+public enum AEnumObjc: Int {
+    case A
+    case B
+    case C
+}
--- G/G/G.swift
+++ G/G/G.swift
@@ -19,3 +19,23 @@ public struct G {
     public static var textC: String { C.text }
     public static var textF: String { FStruct.text }
 }
+
+// Class which is exposed to @objc and uses various A types
+@objcMembers
+public class GObjcClass: NSObject {
+
+    // works because AObjc is forward declared with @class in G-Swift.h
+    public func aClassWorks(_ obj: AObjC) {
+        print("works")
+    }
+
+    // works because AObjc is forward declared with @protocol in G-Swift.h
+    public func aProtocolWorks(_ prot: AProtObjc) {
+        print(prot.text)
+    }
+
+    // doesn't work because enum cannot be forward declared in G-Swift.h -> changes forward declarations into @import A;
+    public func aEnumDoesntWork(_ enum: AEnumObjc) {
+        print("no work")
+    }
+}
